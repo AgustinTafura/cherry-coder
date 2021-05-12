c2e7615a6ff57e6f4b94fcddb7638e319caa298b
AgustinTafura <agustintafura@hotmail.com>
2021-05-12 10:33:24 -0300

    correccion path img

--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -10,17 +10,17 @@ const SectionsContainer = () => {
                 </div>
                 <div className="col-md-5">
                     <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500"
-                    height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500"
-                    preserveAspectRatio="xMidYMid slice" focusable="false">
-                    <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
+                        height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500"
+                        preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>Placeholder</title>
+                        <rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
                     </svg>
-        
+
                 </div>
             </div>
-    
-            <hr className="featurette-divider"/>
-    
+
+            <hr className="featurette-divider" />
+
             <div className="row featurette">
                 <div className="col-md-7 order-md-2">
                     <h2 className="featurette-heading">Oh yeah, it’s that good. <span className="text-muted">See for yourself.</span></h2>
@@ -29,16 +29,16 @@ const SectionsContainer = () => {
                 </div>
                 <div className="col-md-5 order-md-1">
                     <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500"
-                    height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500"
-                    preserveAspectRatio="xMidYMid slice" focusable="false">
-                    <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
+                        height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500"
+                        preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>Placeholder</title>
+                        <rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
                     </svg>
                 </div>
             </div>
-    
-            <hr className="featurette-divider"/>
-    
+
+            <hr className="featurette-divider" />
+
             <div className="row featurette">
                 <div className="col-md-7">
                     <h2 className="featurette-heading">And lastly, this one. <span className="text-muted">Checkmate.</span></h2>
@@ -48,15 +48,15 @@ const SectionsContainer = () => {
                 </div>
                 <div className="col-md-5">
                     <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500"
-                    height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500"
-                    preserveAspectRatio="xMidYMid slice" focusable="false">
-                    <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
+                        height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500"
+                        preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>Placeholder</title>
+                        <rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
                     </svg>
-        
+
                 </div>
             </div>
-            <hr className="featurette-divider"/>
+            <hr className="featurette-divider" />
         </div>
 
     )
