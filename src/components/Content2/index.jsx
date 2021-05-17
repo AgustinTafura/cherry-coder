6d0002f67ce50d9fbe65c48f54881fbf56c0ddd2
Aye Campot <ayecampot@gmail.com>
2021-05-17 19:42:39 -0300

    ajustes media queires

--- src/components/Content2/index.jsx
+++ src/components/Content2/index.jsx
@@ -11,9 +11,9 @@ const ContentBody = () => {
         <h6>Se usan a temperatura ambiente, calientes o frías según la necesidad</h6>
 
       </div>
-      <div className="col-lg-5 col-md-12">
-        <img src={`${process.env.PUBLIC_URL}/assets/img/imagen4.jpg`} />
-      </div>
+
+      <img src={`${process.env.PUBLIC_URL}/assets/img/imagen4.jpg`} className="col-lg-5 imagen" />
+
     </div>
   )
 }
