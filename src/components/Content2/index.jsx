0f2bfaca0b7e3bec93f6da57b6de41b357c6242a
Aye Campot <ayecampot@gmail.com>
2021-05-17 18:39:39 -0300

    Cambios responsive body

--- src/components/Content2/index.jsx
+++ src/components/Content2/index.jsx
@@ -11,7 +11,7 @@ const ContentBody = () => {
         <h6>Se usan a temperatura ambiente, calientes o frías según la necesidad</h6>
 
       </div>
-      <div className="col-lg-5">
+      <div className="col-lg-5 col-md-12">
         <img src={`${process.env.PUBLIC_URL}/assets/img/imagen4.jpg`} />
       </div>
     </div>
